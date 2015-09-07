9e6ab6ebacc0340ab7b02932f65b34937dee1f3f
hz7680 <346187456@qq.com>
2015-09-07 14:26:24 +0800

    完成宠物页面-lv1
    完成宠物页面-lv1

--- game/templates_c/fd80f52d6c32dbaa10e722cc7450d325abf6ff6e.file.myPat.html.php
+++ game/templates_c/fd80f52d6c32dbaa10e722cc7450d325abf6ff6e.file.myPat.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.20, created on 2015-09-07 11:59:39
+<?php /* Smarty version Smarty-3.1.20, created on 2015-09-07 14:23:33
          compiled from "./templates/myPat.html" */ ?>
 <?php /*%%SmartyHeaderCode:116522939755eced47705a05-37893906%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'fd80f52d6c32dbaa10e722cc7450d325abf6ff6e' => 
     array (
       0 => './templates/myPat.html',
-      1 => 1441598377,
+      1 => 1441606999,
       2 => 'file',
     ),
   ),
@@ -46,7 +46,8 @@ $_valid = $_smarty_tpl->decodeProperties(array (
                 </ul>
                 <img src="../public/image/panda-level.png" />
             </div>
-            <div class="bottombox">
+            <img src="../public/image/pat/1.gif" class="pat_level1"/>
+            <div class="bottombox" id="bottombox">
                 <img src="../public/image/bamboo.png" class="bamboo"/>
                 <div class="box">
                     <div class="insidebox">
@@ -86,6 +87,8 @@ $_valid = $_smarty_tpl->decodeProperties(array (
                 div.style.background = "";
             }
         },false);
+        var bottombox=document.getElementById('bottombox');
+        bottombox.style.width=100+'%';
     </script>
 </body>
 </html><?php }} ?>
