ae354d381d87d7ed55be34e8baf0c8555eb05f92
hz7680 <346187456@qq.com>
2015-09-10 16:50:58 +0800

    test
    test

--- test/test.php
+++ test/test.php
@@ -1,4 +1,8 @@
 <?php
     require_once '../common/set_inc.php';
-    $patService=new PatService();
-    echo var_dump($patService->getGoldCoin('123456'));
+    $arr = array();
+    $arr['key1'] = 'val1';
+    $arr['key2'] = 'val2';
+    $str = serialize($arr);
+    echo $str . '<br/>';
+    echo var_dump(unserialize($str));
